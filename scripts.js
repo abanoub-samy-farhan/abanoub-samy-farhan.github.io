a8300ef86bf471a73c356386df169b76351dedd0
abanoubfarhan <abanoubsamy2341@gmail.com>
2024-07-27 18:16:52 +0300

    first Deployment trial

---
+++ scripts.js
@@ -0,0 +1,26 @@
+import {Swiper} from './node_modules/swiper/swiper-bundle.mjs';
+
+const swiper = new Swiper('.slider-warpper', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true, 
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+      1200: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      }
+    },
+    touchEventsTarget: 'wrapper'
+  });
